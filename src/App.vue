71bc87946969176f760a8ba4c7f8d58c48398a70
ltpbje <19836788252@163.com>
2024-01-15 19:54:48 +0800

    '提交错误代码的演示'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-console.log(gaga)
+console.log(lele)
 </script>
 
 <template>
