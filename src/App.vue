d7cb91c8f4a48210fcec51fc37780f062b890c54
ltpbje <19836788252@163.com>
2024-01-16 22:28:36 +0800

    VueRouter4 路由代码练习完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+// import { useRouter, useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
+const Router = useRouter()
+// const Route = useRoute()
+const toList = () => {
+  Router.push('/list')
+}
+</script>
 
 <template>
   <div>我是APP</div>
+  <button @click="$router.push('/home')">首页</button>
+  <button @click="toList()">列表</button>
 </template>
 
 <style scoped></style>
