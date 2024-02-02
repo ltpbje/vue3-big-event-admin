2747da1ab73f6741a8a9f0d3adb0c5d9305f7d2d
ltpbje <19836788252@163.com>
2024-02-02 22:28:42 +0800

    文章分类渲染完全完成

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { Edit, Delete } from '@element-plus/icons-vue'
-
 import { artGetChannelList } from '@/api/article.js'
 import { ref } from 'vue'
+const loading = ref(false)
 const channelList = ref([])
 const getChannelList = async () => {
+  loading.value = true
+  // 请求获取频道列表
   const res = await artGetChannelList()
+  loading.value = false
   channelList.value = res.data.data
+  channelList.value = []
   console.log(res.data.data)
 }
 getChannelList()
@@ -21,10 +25,10 @@ getChannelList()
     <template v-slot:extra>
       <el-button type="primary">添加分类</el-button>
     </template>
-    <el-table :data="channelList" style="width: 100%">
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
       <el-table-column prop="date" label="序号" width="100" type="index" />
-      <el-table-column prop="cate_name" label="分类名称" width="180" />
-      <el-table-column prop="cate_alias" label="分类别名" width="180" />
+      <el-table-column prop="cate_name" label="分类名称" />
+      <el-table-column prop="cate_alias" label="分类别名" />
       <el-table-column prop="address" label="操作" width="100">
         <template #default="{ row, $index }">
           <el-button
@@ -41,6 +45,10 @@ getChannelList()
           ></el-button>
         </template>
       </el-table-column>
+
+      <template #empty>
+        <el-empty description="没有数据" />
+      </template>
     </el-table>
   </page-container>
 </template>
