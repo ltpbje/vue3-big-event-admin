91eeabd1f2bdf04e2b1b4ecad7c6beefbcb2fa90
ltpbje <19836788252@163.com>
2024-02-07 17:23:00 +0800

    179-Vue3大事件项目-文章管理-静态结构搭建完成

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,3 +1,72 @@
+<script setup>
+import { Edit, Delete } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+// 假数据
+const articleList = ref([
+  {
+    id: 5961,
+    title: '新的文章啊',
+    pub_date: '2022-07-10 14:53:52.604',
+    state: '已发布',
+    cate_name: '体育'
+  },
+  {
+    id: 5962,
+    title: '新的文章啊',
+    pub_date: '2022-07-10 14:54:30.904',
+    state: null,
+    cate_name: '体育'
+  }
+])
+const onEdit = (row) => {
+  console.log('编辑', row)
+}
+</script>
 <template>
-  <div>文章管理</div>
+  <page-container title="添加文章">
+    <el-form inline>
+      <el-form-item style="width: 200px" label="文章分类:">
+        <el-select>
+          <el-option label="新闻" value="111"></el-option>
+          <el-option label="体育" value="222"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="发布状态">
+        <el-select>
+          <el-option label="已发布" value="已发布"></el-option>
+          <el-option label="草稿" value="草稿"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">搜索</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="articleList" style="width: 100%">
+      <el-table-column :value="articleList" label="文章标题">
+        <template #default="{ row }">
+          <el-link type="primary" :underline="false">{{ row.title }}</el-link>
+        </template>
+      </el-table-column>
+      <el-table-column label="分类" prop="cate_name"></el-table-column>
+      <el-table-column label="发表时间" prop="pub_date"></el-table-column>
+      <!-- 利用作用域插槽row可以获取当前行的数据 => v-for遍历item- -->
+      <el-table-column label="操作">
+        <template #default="{ row }">
+          <el-button
+            type="success"
+            @click="onEdit(row)"
+            :icon="Edit"
+            circle
+          ></el-button>
+          <el-button
+            type="danger"
+            @click="onDel(row)"
+            :icon="Delete"
+            circle
+          ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </page-container>
 </template>
