5960d228fd4ab8945f23590dfd5606423f75db2a
ltpbje <19836788252@163.com>
2024-02-26 20:12:09 +0800

    AI综合实战-个人中心-基本资料个人信息回显完成

--- src/views/user/UserProfile.vue
+++ src/views/user/UserProfile.vue
@@ -1,69 +1,49 @@
-<template>
-  <!-- 测试 -->
-  <el-form
-    ref="form"
-    :model="form"
-    :rules="rules"
-    label-width="100px"
-    class="demo-ruleForm"
-  >
-    <el-form-item label="登录名称">
-      <el-input v-model="form.loginName" disabled></el-input>
-    </el-form-item>
-    <el-form-item label="用户昵称" prop="nickname">
-      <el-input v-model="form.nickname"></el-input>
-    </el-form-item>
-    <el-form-item label="用户邮箱" prop="email">
-      <el-input v-model="form.email"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm">提交修改</el-button>
-    </el-form-item>
-  </el-form>
-</template>
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+import { ref } from 'vue'
+import { useUserStore } from '@/stores'
+// 是在使用仓库中数据的初始值（无需响应式）解构无问题
+const {
+  user: { username, nickname, email, id }
+} = useUserStore()
+const formModel = ref({
+  username,
+  id,
+  nickname,
+  email
+})
 
-<script>
-export default {
-  data() {
-    return {
-      form: {
-        loginName: '默认登录名称',
-        nickname: '',
-        email: ''
-      },
-      rules: {
-        nickname: [
-          { required: true, message: '请输入用户昵称', trigger: 'blur' },
-          { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
-        ],
-        email: [
-          { required: true, message: '请输入用户邮箱', trigger: 'blur' },
-          { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
-        ]
-      }
-    }
-  },
-  methods: {
-    submitForm() {
-      this.$refs.form.validate((valid) => {
-        if (valid) {
-          // 表单验证通过，可以提交数据
-          console.log('表单验证通过，提交数据:', this.form)
-        } else {
-          // 表单验证不通过，给出提示
-          this.$message.error('表单验证失败，请检查输入')
-          return false
-        }
-      })
-    }
-  }
+const rules = {
+  nickname: [
+    { required: true, message: '请输入昵称', trigger: 'blur' },
+    { min: 2, max: 10, message: '昵称长度必须在2-10位之间', trigger: 'blur' }
+  ],
+  email: [
+    { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+    { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+  ]
 }
+
+const formRef = ref(null)
 </script>
 
-<style scoped>
-.demo-ruleForm {
-  max-width: 400px;
-  margin: 0 auto;
-  padding-top: 50px;
-}
-</style>
+<template>
+  <page-container>
+    <el-form :model="formModel" :rules="rules" ref="formRef">
+      <el-form-item label="登录名称" prop="username">
+        <el-input v-model="formModel.username" disabled></el-input>
+      </el-form-item>
+      <el-form-item label="用户昵称" prop="nickname">
+        <el-input v-model="formModel.nickname"></el-input>
+      </el-form-item>
+      <el-form-item label="用户邮箱" prop="email">
+        <el-input v-model="formModel.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">提交修改</el-button>
+      </el-form-item>
+    </el-form>
+  </page-container>
+</template>
+
+<style scoped></style>
